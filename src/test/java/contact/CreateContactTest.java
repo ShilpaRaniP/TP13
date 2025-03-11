6d01dac38f856c53b5c289d3d9157e3de8cdcf61
ShilpaRaniP <smarts.shilpa@gmail.com>
2025-03-11 15:07:25 +0530

    added *s

--- src/test/java/contact/CreateContactTest.java
+++ src/test/java/contact/CreateContactTest.java
@@ -5,6 +5,7 @@ public class CreateContactTest {
 	public static void main(String[] args) {
 		System.out.println("CreateContactTest executed");
 		System.out.println("Updated");
+		System.out.println("*******");
 
 	}
 
